2ccfee6a6a493a105ffc866225d81b313493e5d7
kolambkar11 <kolambkarashutosh@gmail.com>
2024-12-17 09:48:49 +0530

    working on UI

---
+++ app/components/common/app-sidebar/Appsidebar.tsx
@@ -0,0 +1,73 @@
+import {
+  MailOpen,
+  File,
+  Settings,
+  SkullIcon,
+  LayoutDashboard,
+} from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+
+const items = [
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Complaints",
+    url: "/complaints",
+    icon: SkullIcon,
+  },
+  {
+    title: "Reports",
+    url: "reports",
+    icon: File,
+  },
+  {
+    title: "Letters",
+    url: "letters",
+    icon: MailOpen,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
